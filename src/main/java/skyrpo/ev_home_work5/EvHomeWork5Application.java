a9fbe362365649802727ce002772880a55360727
katya <kotenokches@rambler.ru>
2022-03-25 16:26:37 +0500

    Первый коммит

---
+++ src/main/java/skyrpo/ev_home_work5/EvHomeWork5Application.java
@@ -0,0 +1,13 @@
+package skyrpo.ev_home_work5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvHomeWork5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvHomeWork5Application.class, args);
+	}
+
+}
---
+++ src/test/java/skyrpo/ev_home_work5/EvHomeWork5ApplicationTests.java
@@ -0,0 +1,13 @@
+package skyrpo.ev_home_work5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvHomeWork5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
